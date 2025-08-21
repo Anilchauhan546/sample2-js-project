eb6df55a54580f0d8bb896bb1b48bffbb67f467e
Anilchauhan546 <moodavathanil2003@gmail.com>
2025-08-21 21:36:13 +0530

    Initial commit - added index2.html and index2.js

---
+++ index2.js
@@ -0,0 +1,7 @@
+let myname = "Anil";
+let date= new Date().toLocaleDateString();
+
+console.log(`Hello, ${myname}! Today's date is ${date}.`);
+function sayHello() {
+    alert(`Hello, ${myname}! Today's date is ${date}.`);
+}
